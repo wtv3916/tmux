b9e7fd836ff0199262ca5333bd9bdb1135bc73e0
WeisMeng <wtv3916@users.noreply.github.com>
2017-03-28 16:43:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,15 @@ PURPLE='\033[1;35m'
 WHITE='\033[0;37m'
 YELLOW='\033[1;33m'
 
-echo -e "${PURPLE}Installing .tmux.conf...${YELLOW}"
+echo -e "${PURPLE}Installing tmux...${YELLOW}"
 ln -s ~/.tmux/tmux.conf ~/.tmux.conf
 
 if [ "$?" -ne "0" ]; then
-  echo -e "${PURPLE}~/.tmux.conf already exists, removing..."
+  echo -e "${PURPLE}~/tmux already exists, removing..."
   rm ~/.tmux.conf
   ln -s ~/.tmux/tmux.conf ~/.tmux.conf
 fi
 
 echo -e "${PURPLE}Setting tmux source...${WHITE}"
-tmux source-file ~/.tmux.conf > /dev/null
+tmux source-file ~/tmux > /dev/null
 echo -e "${CYAN}Success!${WHITE}"
